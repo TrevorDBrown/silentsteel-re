6cc7e099922a27905eaff229ae026fa86f857e81
TrevorDBrown <trevordylanbrown@gmail.com>
2025-08-10 23:15:43 -0500

    Update play_game.py
    - Attempted to improve readability.

--- play_game.py
+++ play_game.py
@@ -6,20 +6,20 @@ import os
 import argparse as ap
 import play_media as pm
 
-# Keywords
-CONTINUE = -1   # Advance to next resource/scene.
-END_GAME = -2
-
-# Terminal Control
-UP_ONE_LINE = "\033[A" # up 1 line
-DELETE_LINE = "\033[K" # clear line
-
 # My sloppy workaround for passing this data around.
 # TODO: make this better!
 global media_bundle
 global active_media
 
-class bcolors:
+class game_states:
+    CONTINUE = -1   # Advance to next resource/scene.
+    END_GAME = -2
+
+class terminal_control:
+    UP_ONE_LINE = "\033[A" # up 1 line
+    DELETE_LINE = "\033[K" # clear line
+
+class terminal_text_styling:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKCYAN = '\033[96m'
@@ -31,7 +31,7 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-def split_lines(data):
+def split_lines(data) -> list:
     parts = []
     string = ""
     while (len(data) > 0):
@@ -53,27 +53,27 @@ def split_lines(data):
     return parts
 
 
-def execute_instruction(instruction: str, game_options: dict):
+def execute_instruction(instruction: str, game_options: dict) -> int:
     global media_bundle
     global active_media
 
     # Split instruction into instruction part, and subtitle part.
-    instruction_parts = instruction.split(";")
+    instruction_parts: list[str] = instruction.split(";")
 
     # If the split results in two or more parts, the second part represents a subtitle, which should be displayed.
     if (len(instruction_parts) > 1 and len(instruction_parts[1].strip()) > 0):
-        sys.stdout.write(f"{UP_ONE_LINE}{DELETE_LINE}")     # TODO: figure out if something like this can be done with a print statement.
+        sys.stdout.write(f"{terminal_control.UP_ONE_LINE}{terminal_control.DELETE_LINE}")     # TODO: figure out if something like this can be done with a print statement.
         print(f"<< {instruction_parts[1].strip()}")
 
     # Get the actual instruction.
-    instruction = instruction_parts[0].strip()
+    instruction: str = instruction_parts[0].strip()
 
-    # DEBUG: print instruction.
-    # print(f"{bcolors.OKGREEN}Current Full Instruction: {instruction}{bcolors.ENDC}")
+    if (game_options["debug"]):
+        print(f"{terminal_text_styling.OKGREEN}Current Full Instruction: {instruction}{terminal_text_styling.ENDC}")
 
     if (instruction[0:2] == "?v"):
         # Believed to be entry point for game. No action necessary.
-        return CONTINUE
+        return game_states.CONTINUE
 
     elif (instruction[0:3] in ("?r$")):
         # Instruction for Video Clip Playback
@@ -81,11 +81,11 @@ def execute_instruction(instruction: str, game_options: dict):
 
     elif (instruction[0:3] in ("?R$")):
         # Instruction for Video Clip Playback Selection based on Censorship Level
-        sub_instruction_parts = instruction[3:].split(",")
+        sub_instruction_parts: list[str] = instruction[3:].split(",")
 
         for i, sub_instruction_part in enumerate(sub_instruction_parts):
             if (not game_options["salty_language"] and (i == 0)):
-                # "Family Friendly" video needs to play. Skip sub-instruction.
+                # "Family Friendly" video needs to play. Skip the current sub-instruction.
                 continue
 
             pm.play_video(active_video_file=active_media["video_file"], video_parts=active_media["video_parts"], idx=int(sub_instruction_part))
@@ -101,45 +101,44 @@ def execute_instruction(instruction: str, game_options: dict):
             case "?g0100":      # TODO: this isn't referenced in STEEL.EXE, so unsure what it should actually be...
                 # Disc 1
                 active_media = media_bundle["disc_1"]
-                print(f"{bcolors.OKGREEN}Swapped to Media from Disc 1{bcolors.ENDC}")
+                print(f"{terminal_text_styling.OKGREEN}Swapped to Media from Disc 1{terminal_text_styling.ENDC}")
 
             case "?g0200":
                 # Disc 2
                 active_media = media_bundle["disc_2"]
-                print(f"{bcolors.OKGREEN}Swapped to Media from Disc 2{bcolors.ENDC}")
+                print(f"{terminal_text_styling.OKGREEN}Swapped to Media from Disc 2{terminal_text_styling.ENDC}")
 
             case "?g0300":
                 # Disc 3
                 active_media = media_bundle["disc_3"]
-                print(f"{bcolors.OKGREEN}Swapped to Media from Disc 3{bcolors.ENDC}")
+                print(f"{terminal_text_styling.OKGREEN}Swapped to Media from Disc 3{terminal_text_styling.ENDC}")
 
             case "?g0600":
                 # Disc 4
                 active_media = media_bundle["disc_4"]
-                print(f"{bcolors.OKGREEN}Swapped to Media from Disc 4{bcolors.ENDC}")
+                print(f"{terminal_text_styling.OKGREEN}Swapped to Media from Disc 4{terminal_text_styling.ENDC}")
 
         # Return the rest of the instruction.
         return int(instruction[7:])
 
     elif (instruction[0:2] == "?*"):
-        # Not really sure what this is for... yet...
         # TODO: figure out how this instruction works.
-        print(f"{bcolors.BOLD}Unknown instruction: {bcolors.OKBLUE}{instruction}{bcolors.ENDC}")
-        return CONTINUE
+        print(f"{terminal_text_styling.BOLD}Unknown instruction: {terminal_text_styling.OKBLUE}{instruction}{terminal_text_styling.ENDC}")
+        return game_states.CONTINUE
 
     elif (instruction[0:6] == '?s0100'):
         # Instruction to End the Game.
-        return END_GAME
+        return game_states.END_GAME
 
     else:
         # The Instruction is unhandled.
-        print(f"{bcolors.BOLD}Unhandled instruction: {bcolors.OKBLUE}{instruction}{bcolors.ENDC}")
+        print(f"{terminal_text_styling.BOLD}Unhandled instruction: {terminal_text_styling.OKBLUE}{instruction}{terminal_text_styling.ENDC}")
 
-    return CONTINUE
+    return game_states.CONTINUE
 
 
-def play_sequence(sequence: str, game_options: dict):
-    instructions = sequence.strip().split(">")
+def play_sequence(sequence: str, game_options: dict) -> int:
+    instructions: list[str] = sequence.strip().split(">")
 
     for instruction in instructions:
 
@@ -147,108 +146,112 @@ def play_sequence(sequence: str, game_options: dict):
 
         if (len(instruction) > 0):
 
-            ret = execute_instruction(instruction, game_options)
+            ret: int = execute_instruction(instruction, game_options)
 
-            if (ret != CONTINUE):
+            if (ret != game_states.CONTINUE):
                 return ret
 
-    return CONTINUE
+    return game_states.CONTINUE
 
 
-def run_exchange(exchange: list, game_options: dict):
+def run_exchange(exchange: list, game_options: dict) -> int:
 
-    options = {}
-    lastOption = ""
+    options: dict = {}
+    last_option: str = ""
 
     for line in exchange:
         if (line[0] in ["+", "=", "-"]):
             options[line] = []
-            lastOption = line
+            last_option = line
+
         elif (line[0] == ">"):
-            options[lastOption].append(line)
+            options[last_option].append(line)
+
         else:
-            print(f"{bcolors.FAIL}WARNING WRONG EXCHANGE: {line}{bcolors.ENDC}")
+            print(f"{terminal_text_styling.FAIL}WARNING WRONG EXCHANGE: {line}{terminal_text_styling.ENDC}")
 
     # Print Exchange Options (1-3)
     for idx, key in enumerate(options.keys()):
         print(f"{idx+1}. {key[7:]}")
 
-    choice_idx = -1
+    choice_idx: int = -1
 
     while (choice_idx < 0 or choice_idx > 2):
         if (game_options["autoplay"]):
-            # Autoplay is active.
+            # Autoplay is active. Computer will decide.
             choice_idx = rnd.randrange(3)
             print(f"CPU's Choice: {choice_idx+1}")
+
         else:
             choice = input("Your Choice: ")
 
             if (choice.isdigit()):
                 choice_idx = int(choice)-1
 
-    choice_str = list(options.keys())[choice_idx]
+    choice_str: str = list(options.keys())[choice_idx]
 
     print(f">> {choice_str[7:]}")
 
-    audio_idx = choice_str[3:7]
+    audio_idx: str = choice_str[3:7]
 
     if (not game_options["silent_run"]):
         pm.play_audio(active_audio_file=active_media["audio_file"], audio_parts=active_media["audio_parts"], idx=int(audio_idx))
 
     # TODO: determine if this selects the response to the exchange?
-    result_str = rnd.choice(options[choice_str])
+    result_str: str = rnd.choice(options[choice_str])
 
-    instructions = result_str.split(">")
+    instructions: list[str] = result_str.split(">")
 
     for instruction in instructions:
         instruction = instruction.strip()
         if (len(instruction) > 0):
-            ret = execute_instruction(instruction, game_options)
-            if (ret != CONTINUE):
+            ret: int = execute_instruction(instruction, game_options)
+            if (ret != game_states.CONTINUE):
                 return ret
 
-    return CONTINUE
+    return game_states.CONTINUE
 
 
-def play_resource(resource, game_options: dict):
-    lines = split_lines(list(resource))
-    exchange = -1
+def play_resource(resource, game_options: dict) -> int:
+    lines: list = split_lines(list(resource))
+    exchange: int = -1
 
     for scene in lines:
         if (isinstance(scene, str) and scene.startswith("sequence")):
-            ret = play_sequence(scene[8:], game_options)
-            if (ret != CONTINUE):
+            ret: int = play_sequence(scene[8:], game_options)
+            if (ret != game_states.CONTINUE):
                 return ret
 
         elif isinstance(scene, str) and scene.endswith('EXCHANGE'):
             exchange = int(scene[0:-9])
 
         elif exchange > 0 or (isinstance(scene, list) and scene[0][0:1] == "+"):
-            ret = run_exchange(scene, game_options)
-            if (ret != CONTINUE):
+            ret: int = run_exchange(scene, game_options)
+            if (ret != game_states.CONTINUE):
                 return ret
             exchange = -1
 
         else:
-            print(f"{bcolors.BOLD}Unsupported Scene: {bcolors.OKBLUE}{json.dumps(scene)}{bcolors.ENDC}")
+            print(f"{terminal_text_styling.BOLD}Unsupported Scene: {terminal_text_styling.OKBLUE}{json.dumps(scene)}{terminal_text_styling.ENDC}")
 
-    return CONTINUE
+    return game_states.CONTINUE
 
-def main():
+def main() -> None:
     global media_bundle
     global active_media
 
     # Establish arguments.
-    arg_parser = ap.ArgumentParser(prog="Silent Steel Player")
+    arg_parser: ap.ArgumentParser = ap.ArgumentParser(prog="Silent Steel Player")
     arg_parser.add_argument("--promo", "-p", action="store_true", help="Specifies the game will be played in Promo Mode (Disc 1 Only)")
     arg_parser.add_argument("--salty", "-x", action="store_true", help="Specifies whether the uncensored (i.e. \"Salty Language\") or censored (i.e. \"Family Friendly\") version of the game will be played.")
     arg_parser.add_argument("--auto", "-a", action="store_true", help="Specifies if the game will be autoplayed.")
     arg_parser.add_argument("--scene", "-s", type=int, help="Specifies a starting scene. Defaults to first scene.")
     arg_parser.add_argument("--silent_run", action="store_true", help="Specifies whether or not the player speech should play.")
+    arg_parser.add_argument("--debug", "-d", action="store_true", help="Provides verbose, debug information.")
     arg_parser.add_argument("media_path", help="Specifies the path to the media files (i.e. VIDEO1.MPG/AVI, VIDEO1.WAV, VIDEO1.IDX)")
 
     # Parse the arguments.
-    parsed_args = arg_parser.parse_args()
+    parsed_args: ap.Namespace = arg_parser.parse_args()
 
     root_path: str = ""
     if (parsed_args.media_path):
@@ -266,7 +269,8 @@ def main():
         "promo": parsed_args.promo,
         "salty_language": parsed_args.salty,
         "autoplay": parsed_args.auto,
-        "silent_run": parsed_args.silent_run
+        "silent_run": parsed_args.silent_run,
+        "debug": parsed_args.debug
     }
 
     print("Welcome to the Silent Steel Interactive Movie!")
@@ -295,7 +299,7 @@ def main():
         active_media = media_bundle["disc_promo"]
 
     else:
-        # Retail Version (Four discs total, start with Disc 1)
+        # Retail Version (Four discs total)
         media_bundle = {
             "disc_1": {
                 "video": "VIDEO1.AVI",
@@ -347,23 +351,23 @@ def main():
             }
         }
 
+        # Start with Disc 1 media.
         active_media = media_bundle["disc_1"]
 
-    # Fetch the Resource Table from the Executable.
-    steel = nf.NE(executable_file)
-    data_resources = steel.resource_table.resources["DATA"]
+    # Fetch the Resource Table from the executable.
+    steel: nf.NE = nf.NE(executable_file)
+    data_resources: dict = steel.resource_table.resources["DATA"]
 
-    scenes = {}
+    scenes: dict = {}
 
     for resource_id, resource in data_resources.items():
-        data_str = resource.data.read().decode("ascii").rstrip("\x1a \x00")  # there's padding
+        data_str: str = resource.data.read().decode("ascii").rstrip("\x1a \x00")  # there's padding
         scenes[resource_id] = data_str
 
-    resource_ids = list(scenes.keys())
+    resource_ids: list = list(scenes.keys())
 
-    idx = 0
-
-    sceneId = resource_ids[idx]
+    idx: int = 0
+    sceneId: int = resource_ids[idx]
 
     # If a start scene is specified, jump to it.
     if (game_options["start_scene"] >= 0):
@@ -372,9 +376,9 @@ def main():
 
     # Ready to play!
     while True:
-        ret = play_resource(scenes[sceneId], game_options)
+        ret: int = play_resource(scenes[sceneId], game_options)
 
-        if (ret == CONTINUE):
+        if (ret == game_states.CONTINUE):
             # Play next scene, if one exists.
             idx += 1
 
@@ -385,7 +389,7 @@ def main():
                 print("Game Over.")
                 break
 
-        elif (ret == END_GAME):
+        elif (ret == game_states.END_GAME):
             # End the Game.
             print("Game Over.")
             break
