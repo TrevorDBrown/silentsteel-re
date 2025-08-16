ffd7ab1460f367a120df25dd3ce111ed8084d74f
TrevorDBrown <trevordylanbrown@gmail.com>
2025-08-16 01:19:25 -0500

    Moved scripts from misc directory to misc/scripts directory

--- misc/scripts/dump_exe.py
+++ misc/scripts/dump_exe.py
@@ -1,5 +1,5 @@
 import nefile
-steel = nefile.NE('/Volumes/Untitled/STEEL.EXE')
+steel = nefile.NE('C:\\Users\\Trevor Brown\\Desktop\\Silent Steel (1995)\\Disc Content\\Promotional\\STEEL.EXE')
 
 print(steel.header.start_offset)
 print(steel.header._resource_table_offset_from_header_start)
@@ -13,6 +13,11 @@ print(steel.resource_table.resources["DATA"])
 
 data_resources = steel.resource_table.resources["DATA"]
 print(data_resources)
+
 for resource_id, resource in data_resources.items():
-    data_str = resource.data.read().decode("ascii").rstrip("\x1a \x00") # there's padding 
-    print("%i: %s"%(resource_id,data_str) ) 
+    data_str = resource.data.read().decode("ascii").rstrip("\x1a \x00") # there's padding
+
+    print("%i: %s"%(resource_id,data_str) )
+
+    with open("TextDump-MSTEEL.txt", "a") as f:
+        f.write("%i: %s\n\n"%(resource_id,data_str))
