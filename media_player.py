a28a24af6d63bebd60944b37826621dd9788608f
Trevor D. Brown <trevordylanbrown@gmail.com>
2026-06-07 15:55:53 -0400

    Update media_player.py
    - Re-ordered imports.
    - Copilot modified file as well.

--- media_player.py
+++ media_player.py
@@ -1,34 +1,43 @@
+import datetime
 import os
 import struct
-import wave
 from subprocess import run
-import datetime
+import wave
+
 import local_ffmpeg
+
 
 def setup_ffplay() -> str:
     ffplay_path: str = ""
 
     # Check if ffmpeg/ffplay is installed on the system and available on PATH.
-    if (local_ffmpeg.is_installed(None)):
+    if local_ffmpeg.is_installed(None):
         print("Using ffmpeg/ffplay on PATH.")
         return os.path.join(ffplay_path, "ffplay")
 
     # ffmpeg/ffplay is either not installed on the system, or is unavailable on PATH. Check if a local copy exists in the project directory.
     ffplay_path = os.path.join(os.getcwd(), "bin", "ffmpeg")
 
-    if (local_ffmpeg.is_installed(ffplay_path)):
+    if local_ffmpeg.is_installed(ffplay_path):
         print(f"Using ffmpeg/ffplay previously installed at: {ffplay_path}")
         return os.path.join(ffplay_path, "ffplay")
 
     # ffmpeg/ffplay is not installed in project directory. Install a local copy.
-    successful_ffmpeg_install, ffmpeg_install_message = local_ffmpeg.install(ffplay_path)
+    successful_ffmpeg_install, ffmpeg_install_message = local_ffmpeg.install(
+        ffplay_path
+    )
 
-    if (not successful_ffmpeg_install):
+    if not successful_ffmpeg_install:
         print(f"Error installing ffmpeg/ffplay: {ffmpeg_install_message}")
         return ""
 
     # Successful installation of ffplay in project directory.
     return os.path.join(ffplay_path, "ffplay")
+
+
+def shutdown_player(ffplay_path: str) -> None:
+    return
+
 
 def ms_to_timecode(milliseconds: int) -> str:
     """
@@ -44,11 +53,12 @@ def ms_to_timecode(milliseconds: int) -> str:
 
     return f"{hours:02d}:{minutes:02d}:{seconds:02d}.{milliseconds_part:03d}"
 
+
 def parse_idx(filename) -> dict:
     parts: dict = {}
     last_idx: int = -1
 
-    with open(filename, 'rb') as in_file:
+    with open(filename, "rb") as in_file:
         data: bytes = in_file.read(16)
         format: str = data[0:4].decode("ascii")
         length: int = int.from_bytes(data[12:14], "little")
@@ -58,22 +68,23 @@ def parse_idx(filename) -> dict:
         while True:
             i += 1
             idx: int = in_file.read(2)
-            if len(idx) == 0:                # breaks loop once no more binary data is read
+            if len(idx) == 0:  # breaks loop once no more binary data is read
                 break
             idx = int.from_bytes(idx, "little")
-            offset = struct.unpack('I', in_file.read(4))[0]
-            if (idx > 0):
+            offset = struct.unpack("I", in_file.read(4))[0]
+            if idx > 0:
                 parts[idx] = {}
                 parts[idx]["start"] = offset
-            if (last_idx > 0):
+            if last_idx > 0:
                 parts[last_idx]["end"] = offset
             last_idx = idx
 
         return parts
 
 
-def play_audio(active_audio_file: str, audio_parts: dict, idx: int, ffplay_path: str) -> None:
-
+def play_audio(
+    active_audio_file: str, audio_parts: dict, idx: int, ffplay_path: str
+) -> None:
     audio_params: dict = {}
 
     with wave.open(active_audio_file, "rb") as wav:
@@ -82,37 +93,80 @@ def play_audio(active_audio_file: str, audio_parts: dict, idx: int, ffplay_path:
     part = audio_parts[idx]
     start = part["start"]
     end = part["end"]
-    bitrate = audio_params.framerate*audio_params.nchannels*audio_params.sampwidth
+    bitrate = audio_params.framerate * audio_params.nchannels * audio_params.sampwidth
 
     # TODO: this somehow isn't frame accurate (sometimes cuts off split second too early or too late)
-    start_sec = start/bitrate
-    end_sec = end/bitrate
-    duration = end_sec-start_sec
+    start_sec = start / bitrate
+    end_sec = end / bitrate
+    duration = end_sec - start_sec
     seek = "%.2f" % start_sec
     duration_str = "%.2f" % duration
 
     # Play the audio.
-    run([ffplay_path, '-hide_banner', '-loglevel', 'warning', '-nodisp', '-autoexit', '-i', active_audio_file, '-ss', seek, '-t', duration_str])
+    run(
+        [
+            ffplay_path,
+            "-hide_banner",
+            "-loglevel",
+            "warning",
+            "-nodisp",
+            "-autoexit",
+            "-i",
+            active_audio_file,
+            "-ss",
+            seek,
+            "-t",
+            duration_str,
+        ]
+    )
 
     return
 
 
-def play_video(active_video_file: str, video_parts: dict, idx: int, ffplay_path: str) -> None:
-
-    if (idx in video_parts):
+def play_video(
+    active_video_file: str, video_parts: dict, idx: int, ffplay_path: str
+) -> None:
+    if idx in video_parts:
         part = video_parts[idx]
         start = part["start"]
         end = part["end"]
-        with open(active_video_file, 'rb') as fin:
+        with open(active_video_file, "rb") as fin:
             fin.seek(start)
 
-            if (active_video_file.endswith("MPG")):
+            if active_video_file.endswith("MPG"):
                 # Play as MPEG video.
-                run([ffplay_path, '-hide_banner', '-vf', 'scale=-1:480', '-loglevel', 'warning', '-autoexit', '-'], input=fin.read(end - start))
+                run(
+                    [
+                        ffplay_path,
+                        "-hide_banner",
+                        "-vf",
+                        "scale=-1:480",
+                        "-loglevel",
+                        "warning",
+                        "-autoexit",
+                        "-",
+                    ],
+                    input=fin.read(end - start),
+                )
 
-            elif (active_video_file.endswith("AVI")):
+            elif active_video_file.endswith("AVI"):
                 # Play as AVI video.
-                run([ffplay_path, '-ss', ms_to_timecode(start), '-t', ms_to_timecode(end - start), active_video_file, '-hide_banner', '-vf', 'scale=-1:480', '-loglevel', 'warning', '-autoexit'])
+                run(
+                    [
+                        ffplay_path,
+                        "-ss",
+                        ms_to_timecode(start),
+                        "-t",
+                        ms_to_timecode(end - start),
+                        active_video_file,
+                        "-hide_banner",
+                        "-vf",
+                        "scale=-1:480",
+                        "-loglevel",
+                        "warning",
+                        "-autoexit",
+                    ]
+                )
 
             else:
                 print(f"WARNING: Video format not handled {idx}")
@@ -120,4 +174,3 @@ def play_video(active_video_file: str, video_parts: dict, idx: int, ffplay_path:
         print(f"WARNING: Video not found {idx}")
 
     return
-
